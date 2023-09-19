87ca9173bd37019c7f9af9f687004225a18ceb3e
coffeepudding97 <118414518+coffeepudding97@users.noreply.github.com>
2023-09-19 18:14:56 +0900

    Update ui_common.js

--- portfolio/js/ui_common.js
+++ portfolio/js/ui_common.js
@@ -2,36 +2,5 @@ $(document).ready(function(){
   $('.txt_wrap h2').addClass('on');
 });
 
-$(function () {
-
-
-    //페이지 넘기기
-    var mHtml = $("html");
-    var page = 1;
-
-
-    mHtml.animate({scrollTop : 0},10);
-
-    $(window).on("wheel", function(e) {
-    if(mHtml.is(":animated")) return;
-    if(e.originalEvent.deltaY > 0) {
-        if(page == 5) return;
-        page++;
-    } else if(e.originalEvent.deltaY < 0) {
-        if(page == 1) return;
-        page--;
-    }
-    var posTop =(page-1) * $(window).height();
-    mHtml.animate({scrollTop : posTop});
-    })
-
-  $('.gnb li>a').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
-  })
-
-       
-  
-});
 
 
